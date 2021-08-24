8b0e2f84172da226bfe2d9ca329593c93eec51e5
Rose Wisotzky <rose@rosewisotzky.com>
2021-08-24 12:29:39 -0500

    event handler console logs message when select changes

--- scripts/pleaEventHandler.js
+++ scripts/pleaEventHandler.js
@@ -2,7 +2,12 @@ export const pleaEventHandler = (event) => {
     console.log("u clicked me")
     let input = document.querySelector(".plea_box")
     let inputVal = input.value
+    
     let displayArea = document.querySelector(".plea_display")
     console.log(inputVal)
     displayArea.innerHTML = inputVal;
+}
+
+export const howManyTimes = (event) => {
+    console.log("oops u got me")
 }
