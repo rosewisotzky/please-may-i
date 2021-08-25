6f3eddcfedfd0d6ddff073135546ab60931aba33
Rose Wisotzky <rose@rosewisotzky.com>
2021-08-25 09:46:56 -0500

    added some comments

--- scripts/pleaEventHandler.js
+++ scripts/pleaEventHandler.js
@@ -11,11 +11,11 @@ export const pleaEventHandler = (event) => {
     displayArea.innerHTML = "";
 
     for (let i = 0; i < pleaAmount; i++) {
+        //declaring the randomNumber inside of the loop -- with every iteration there will be a new random number to compare against the index value of each 'plea'. if it's a match then we go uppercase and if not we go lowercase.
         let randomNum = Math.floor((Math.random() * pleaAmount) + 1);
         console.log("random number", randomNum);
         console.log("index no. in loop", i);
         if (i === randomNum) {
-
             displayArea.innerHTML += `<br> ${inputVal.toUpperCase()} `;
         }
         else {
